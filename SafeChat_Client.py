17ed725832f149269c1ef27d0eecde9887389e78
Welelow <lerotim@tsn.at>
2025-05-16 00:24:57 +0200

    Trennung von Anzeige und Kommunikation

--- SafeChat_Client.py
+++ SafeChat_Client.py
@@ -5,82 +5,120 @@ import SafeChat_Verschlüsselung as crypt
 #nur eine connection wird benötigt wenn man mit einem thread jeweils nur empfangt und der andere sendet
 
 # Funktion erstellen für key_exchange und Variable mit IP und jeweiligen Schlüssel (Funktion wird immer aufgerufen, wenn man mit einem anderen Client schreibt).
-# Verschlüsselung muss noch ausgebaut werden, sodass man nicht gleichzeitig absenden muss. (mit Thread)
 
 
-# glogbale Variable mit keys? Tupel: {IP: key}   (ohne extra $$$)
-def receive_data(sock): # nicht gleichzeitig senden müssen???????????????????
-    while True:
+class ChatProcessor:
+    def __init__(self):
+        self.final_keys = {}  # {IP: key} (später halt Benutzer)
+        self.sock_lock = threading.Lock()
+        self.private_key = crypt.DH_generate_private_key()
+        self.tcp_s = None
+
+    def find_server(self):
+        # Socket erstellen
+        udp_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        udp_s.settimeout(5)
+
+        # Broadcast: "SafeChat_Client". 
+        udp_s.sendto(b"SafeChat_Client", ("<broadcast>", 5005))
+
         try:
-            received_data = sock.recv(1024).decode()
-            if received_data:
-                source, message = received_data.split("$", 1)
-                print(f"Message: {message}")
-                if source in final_keys:
-                    decrypto = crypt.Str_encrypt(message, final_keys[source])
-                    print(f"\n Nachricht von {source}: {decrypto}")
+            # Warten auf Antwort von dem Server
+            data, server = udp_s.recvfrom(1024)
+            if data == b"SafeChat_Server":
+                print("Server gefunden unter IP:", server[0])
+                
+                # Hier wird die TCP-Verbindung aufgebaut
+                self.tcp_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.tcp_s.connect((server[0], 3001))
+                
+                thread = threading.Thread(target=self.receive_data, args=(self.tcp_s,), daemon=True)
+                thread.start()
+                
+                return True
+        except socket.timeout:
+            print("Kein Server gefunden – Timeout.")
+            return False
+
+    def receive_data(self, sock):
+        while True:
+            try:
+                received_data = sock.recv(1024).decode()
+                if received_data:
+                    source, message = received_data.split("$", 1)
+                    print(f"Message: {message}")
+                    if source in self.final_keys:
+                        decrypto = crypt.Str_encrypt(message, self.final_keys[source])
+                        print(f"\n Nachricht von {source}: {decrypto}")
+                    else:
+                        print(f"\n per2change (receive_data): {message}")
+
+                        # sende eigene Änderung
+                        per1change = crypt.DH_calculate_change(self.private_key)
+                        print(f"per1change (receive_data): {per1change}")
+                        per1change = str(per1change)
+                        dh_data = source + "$" + per1change
+                        while True:
+                            try:
+                                self.sock_lock.acquire()
+                                self.tcp_s.sendall(dh_data.encode())
+                                break
+                            finally:
+                                self.sock_lock.release()
+
+                        per2change = int(message)
+                        self.final_keys[source] = crypt.DH_calculate_change(self.private_key, per2change)
+                        print(f"Final key: {self.final_keys[source]}")
                 else:
-                    print(f"\n per2change (receive_data): {message}")
-                    
-                    #senden von eigener Änderung
-                    per1change = crypt.DH_calculate_change(private_key)
-                    print(f"per1change (receive_data): {per1change}")
-                    per1change = str(per1change)
-                    dh_data = source + "$" + per1change
-                    while True:
-                        try:
-                            sock_lock.acquire() #lock
-                            tcp_s.sendall(dh_data.encode())
-                            break
-                        finally:
-                            sock_lock.release()
-                    
-                    #finalen Key berechnen
-                    per2change = int(message)
-                    final_keys[source] = crypt.DH_calculate_change(private_key, per2change)
-                    print(f"Final key: {final_keys[source]}")
-            else:
+                    break
+            except Exception as e:
+                print("Fehler beim Empfangen:", e)
                 break
-        except Exception as e:
-            print("Fehler beim Empfangen:", e)
-            break
-        
 
-#Globale Variable mit Keys
-global final_keys
-final_keys = {} # {IP: Key} (später halt Benutzer)
+    def send_data(self, destination_client, message): 
+        if destination_client not in self.final_keys: # DH falls noch kein Key vorhanden
+            #macht und sendet erste Änderung
+            per1change = crypt.DH_calculate_change(self.private_key)
+            print(f"per1change: {per1change}")
+            per1change = str(per1change)
+            dh_data = destination_client + "$" + per1change
+            while True:
+                try:
+                    self.sock_lock.acquire() #lock
+                    self.tcp_s.sendall(dh_data.encode())
+                    break
+                finally:
+                    self.sock_lock.release()
 
-#Lock erstellen für senden in receive_data()  (und später send_data)
-global sock_lock
-sock_lock = threading.Lock()
+            #empfängt geänderten Public Key von anderem Benutzer
+            received_change = self.tcp_s.recv(1024).decode() #hier kann noch ein Error auftreten, wenn hier von einem anderen Benutzer empfangen wird wie davor
+            source, per2change = received_change.split("$", 1)
+            print(f"per2change: {per2change}")
 
-# DH
-global private_key
-private_key = crypt.DH_generate_private_key()
-print(f"private_key: {private_key}")
+            #macht zweite Änderung und schreibt Key in dictionary
+            per2change = int(per2change)
+            self.final_keys[source] = crypt.DH_calculate_change(self.private_key, per2change)
+            print(f"Final key: {self.final_keys[source]}")
 
-# Socket erstellen
-udp_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-udp_s.settimeout(5)
+        crypto = crypt.Str_encrypt(message, self.final_keys[destination_client])
+        data = destination_client + "$" + crypto
 
-# Broadcast: "SafeChat_Client". 
-udp_s.sendto(b"SafeChat_Client", ("<broadcast>", 5005))
+        self.sock_lock.acquire()
+        self.tcp_s.sendall(data.encode())
+        self.sock_lock.release()
 
-try:
-    # Warten auf Antwort von dem Server
-    data, server = udp_s.recvfrom(1024)
-    if data == b"SafeChat_Server":
-        print("Server gefunden unter IP:", server[0])
+class ChatInterface:
+    def __init__(self, processor: ChatProcessor):
+        self.processor = processor
 
-        # Hier wird die TCP-Verbindung aufgebaut
-        tcp_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        tcp_s.connect((server[0], 3001))
-        
-        receive_thread = threading.Thread(target=receive_data, args=(tcp_s,), daemon=True)
-        receive_thread.start()
-        
-        
+    def start(self):
+        print(f"private_key: {self.processor.private_key}")
+
+        # Wichtig, weil das Programm bei keinen Server nicht einfach weiterlaufen soll
+        if not self.processor.find_server():
+            return
+
         while True:
             while True:
                 destination_client = input("Empfänger eingeben (z.B. 127.0.0.1) oder exit zum beenden: ")
@@ -88,46 +126,17 @@ try:
                     print("Es darf kein $ in der Adresse sein!")
                 else:
                     break
+
             if destination_client == "exit":
                 break
-            
-            
-            if destination_client not in final_keys: # DH falls noch kein Key vorhanden
-                #macht und sendet erste Änderung
-                per1change = crypt.DH_calculate_change(private_key)
-                print(f"per1change: {per1change}")
-                per1change = str(per1change)
-                dh_data = destination_client + "$" + per1change
-                while True:
-                    try:
-                        sock_lock.acquire() #lock
-                        tcp_s.sendall(dh_data.encode())
-                        break
-                    finally:
-                        sock_lock.release()
-                
-                #empfängt geänderten Public Key von anderem Benutzer
-                received_change = tcp_s.recv(1024).decode()  #hier kann noch ein Error auftreten, wenn hier von einem anderen Benutzer empfangen wird wie davor
-                source, per2change = received_change.split("$", 1) 
-                print(f"per2change: {per2change}")
-                
-                #macht zweite Änderung und schreibt Key in dictionary
-                per2change = int(per2change)
-                final_keys[source] = crypt.DH_calculate_change(private_key, per2change)
-                print(f"Final key: {final_keys[source]}")
-                
-            
-            message = input("Nachricht eingeben: ")
-            
-            crypto = crypt.Str_encrypt(message, final_keys[destination_client])
-            
-            data = destination_client + "$" + crypto
-            
-            sock_lock.acquire()
-            tcp_s.sendall(data.encode())
-            sock_lock.release()
 
-    tcp_s.close()
-    
-except socket.timeout:
-    print("Kein Server gefunden – Timeout.")
+            message = input("Nachricht eingeben: ")
+            self.processor.send_data(destination_client, message)
+
+        self.processor.tcp_s.close()
+
+
+if __name__ == "__main__":
+    processor = ChatProcessor()
+    interface = ChatInterface(processor)
+    interface.start()
